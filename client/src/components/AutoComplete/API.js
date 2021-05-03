ebd5f7a169b36b0af215ee828f768a3dc49f39ff
Damian Norman <damiannorman@Damians-MBP.fios-router.home>
2021-05-03 13:30:35 -0400

    focus

--- client/src/components/AutoComplete/API.js
+++ client/src/components/AutoComplete/API.js
@@ -1,16 +1,19 @@
 import {get, add} from "./SimpleCache";
 
-const API = async (url = '') => {
+const API = async (url = '', setLoading) => {
   const cachedResult = get(url);
 
   if(cachedResult) {
     return cachedResult;
   }
 
+  setLoading(true);
+
   try {
     const response = await fetch(url);
     const json = await response.json();
     add(url, json);
+    //setLoading(false);
     return json;
 
   } catch (err) {
--- client/src/components/AutoComplete/index.js
+++ client/src/components/AutoComplete/index.js
@@ -17,9 +17,25 @@ const StyledContainer = styled.div`
   position: relative;
 `;
 
+const StyledLoading = styled.div`
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: absolute;
+  z-index: 1000;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.75;
+`;
+
 const AutoComplete = (props) => {
   const {baseURL} = props;
   const [filter, setFilter] = useState('');
+  const [loading, setLoading] = useState(false);
   const [selected, setSelected] = useState('');
   const [results, setResults] = useState([]);
   const resultDisplay = React.createRef();
@@ -29,7 +45,7 @@ const AutoComplete = (props) => {
   useEffect(() => {
     const get = async () => {
       const url = `${baseURL}?filter=${filter}`;
-      const reponse = await API(url);
+      const reponse = await API(url, setLoading);
       setResults(reponse);
     };
 
@@ -49,6 +65,7 @@ const AutoComplete = (props) => {
 
   return (
     <StyledContainer>
+      {loading && <StyledLoading>loading</StyledLoading>}
       <AutoCompleteInput ref={input} setFilter={setFilter} filter={filter} selected={selected} setSelected={setSelected} clear={clear}/>
       {(results.length && <AutoCompleteResults ref={resultDisplay}  setSelected={setSelected} results={results} setResults={setResults} clear={clear}/>) || <></> }
     </StyledContainer>
